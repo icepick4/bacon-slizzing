ea1bd7536e65003bcb9ec2cb08bb966409e1d88b
Icepick <remi.jara@laposte.net>
2023-01-22 12:18:22 +0100

    adding button

---
+++ script.js
@@ -0,0 +1,14 @@
+const trustButton = document.getElementById('trust-button');
+
+trustButton.addEventListener('click', () => {
+    trustButton.style.display = 'none';
+    play();
+});
+
+function play() {
+    const body = document.getElementById('body');
+    body.style.backgroundImage = "url('bacon.gif')";
+    body.style.cursor = "default";
+    let audio = document.getElementById("audio");
+    audio.play();
+}
